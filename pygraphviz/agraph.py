c1fd9550abf6995a7c4c7d48c42c8a62d24d738a
Ross Barnowski <rossbar@berkeley.edu>
2022-10-21 14:16:56 -0700

    WIP: Test using newly exposed context fn - expect failure.

--- pygraphviz/agraph.py
+++ pygraphviz/agraph.py
@@ -1485,7 +1485,7 @@ class AGraph:
         if isinstance(prog, str):
             prog = prog.encode(self.encoding)
 
-        gvc = gv.gvContext()
+        gvc = gv.gvContextPlugins()
         retval = gv.gvLayout(gvc, self.handle, prog)
         # gvLayout returns -1 if `prog` is not a valid program.
         # TODO: Check other possible return values from gvLayout
